f4b811c7414cce73c43988b3735ade39bf229539
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-08-17 10:31:30 +0300

    save-frend-account

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Card, Profile} from 'components/Card/Card';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    
+    <Card/>
+    
   </React.StrictMode>
 );
+
